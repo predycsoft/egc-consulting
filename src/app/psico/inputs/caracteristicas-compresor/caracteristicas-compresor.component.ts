b5d0686b53c5281d1781c5a3b5a44db7eed8efb8
predycsoft <predycsoft@gmail.com>
2021-08-14 23:42:13 -0500

    hola

--- src/app/psico/inputs/caracteristicas-compresor/caracteristicas-compresor.component.ts
+++ src/app/psico/inputs/caracteristicas-compresor/caracteristicas-compresor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-interface operatingConditions{
+interface operatingConditions {
   gasHandled: number,
   molecularWeight: number,
   mmscfd: number,
@@ -25,7 +25,7 @@ interface operatingConditions{
   pRatio: number,
   certifiedPoint: number,
   performanceCurve: number,
-} 
+}
 
 @Component({
   selector: 'caracteristicas-compresor',
@@ -34,9 +34,42 @@ interface operatingConditions{
 })
 export class CaracteristicasCompresorComponent implements OnInit {
 
+  operatingConditions: operatingConditions[] = []
+
   constructor() { }
+  init: operatingConditions = {
+    gasHandled: 0,
+    molecularWeight: 0,
+    mmscfd: 0,
+    massFlow: 0,
+    weightFlow: 0,
+    inletVolume: 0,
+    p1: 0,
+    t1: 0,
+    k1: 0,
+    z1: 0,
+    p2: 0,
+    t2: 0,
+    k2: 0,
+    z2: 0,
+    brakeHorsepower: 0,
+    compressorGasPower: 0,
+    rpm: 0,
+    Hpoly: 0,
+    Hisen: 0,
+    Npoly: 0,
+    Nisen: 0,
+    pRatio: 0,
+    certifiedPoint: 0,
+    performanceCurve: 0,
+  }
 
   ngOnInit(): void {
+    console.log("hola")
+    this.operatingConditions = this.operatingConditions.concat(this.init);
+    Object.keys(this.operatingConditions[0]).forEach(key => {
+      console.log(key);
+    })
   }
 
 }
