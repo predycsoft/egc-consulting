0341f36e459b7579740ceaf9eedf34cb02a0e770
predycsoft <predycsoft@gmail.com>
2021-08-15 22:47:46 -0500

    ñpushhhshdfsaldfhsdjklf

--- src/app/psico/inputs/configuracion-tren/configuracion-tren.component.ts
+++ src/app/psico/inputs/configuracion-tren/configuracion-tren.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+interface tren {
+  tag: string,
+  cantEquipos: number,
+  equipos: equipo[],
+}
+
+interface equipo {
+  tag: string,
+  orden: number,
+  tipo: "compresor" | "turbina",
+}
+
 @Component({
   selector: 'configuracion-tren',
   templateUrl: './configuracion-tren.component.html',
@@ -10,7 +22,60 @@ export class ConfiguracionTrenComponent implements OnInit {
   constructor(
   ) { }
 
-  ngOnInit(): void {
+  tipoSimulacion: "real" | "teorica" | "ambas";
+
+
+  nuevo = true
+  tren:tren  = {
+    tag: "",
+    cantEquipos: 0,
+    equipos: [],
   }
 
+  equipo = {
+    tag: "",
+    orden: 0,
+    tipo: "compresor",
+  }
+
+  ngOnInit(): void {
+    if (this.nuevo) {
+      this.inicializarTren();
+    } else {
+      this.cargarTren();
+    }
+  }
+
+  inicializarTren() {
+    this.tipoSimulacion = "ambas";
+    for (let index = 0; index < 4; index++) {
+      this.anexarEquipo("compresor")      
+    }
+  }
+
+  anexarEquipo(tipo: "compresor" | "turbina") {
+    if (this.tren.equipos.length == 0) {
+      tipo = "turbina"
+    }
+    this.tren.equipos.concat({
+      tag: "",
+      orden: this.tren.equipos.length,
+      tipo: tipo,
+    })
+  }
+
+  modificarEquipo(index: number) {
+    this.equipo = this.tren.equipos[index]
+    // abrir ventana
+    // al cerrar ventana guardar this.tren.equipos[index] = this.equipo y volver a reininciar
+  }
+
+  cargarTren() {
+    const tren = "tren";
+    return tren;
+  }
+
+  cargarEquipo(index: number) {
+    return true
+  }
 }
