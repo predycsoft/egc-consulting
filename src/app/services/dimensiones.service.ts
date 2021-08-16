0fec340ae4d8fb44304168995e5cdf83fc83f3d4
predycsoft <predycsoft@gmail.com>
2021-08-15 19:35:41 -0500

    lkfgjdfk

--- src/app/services/dimensiones.service.ts
+++ src/app/services/dimensiones.service.ts
@@ -24,13 +24,81 @@ export class DimensionesService {
   flujo = ["[MMSCFD]", "[ACFM]", "[lbm/min]", "[lbmol/dia]", "[m3/min]", "[m3/sec]"];
 
 
-  transformarTemperatura(valor:number, a: string, b: string): number {
+  transformarTemperatura(valor: number, a: string, b: string): number {
     let c = 0;
-    return valor
+    const kelvin = this.tempToKelvin(valor, a)
+    switch (b) {
+      case "[°F]": {
+        c = (kelvin - 273.15) * 9 / 5 + 32
+        return c
+      }
+      case "[°C]": {
+        c = kelvin - 273.15
+        return c
+      }
+      case "[°K]": {
+        c = kelvin
+        return c
+      }
+      case "[°R]": {
+        c = kelvin * 9 / 5;
+        return c
+      }
+      default: {
+        return c
+      }
+    }
   }
 
-  tempToK(valor: number, ) {
-    return valor
+  tempToKelvin(valor: number, a: string) {
+    let c = 0
+    switch (a) {
+      case "[°F]": {
+        c = (valor + 459.67) * (5 / 9);
+        return c
+      }
+      case "[°C]": {
+        c = valor + 273.15
+        return c
+      }
+      case "[°K]": {
+        c = valor
+        return c
+      }
+      case "[°R]": {
+        c = valor * 5 / 9;
+        return c
+      }
+      default: {
+        return c
+      }
+    }
+  }
+
+  transformarPresion(valor: number, a: string, b: string): number {
+    let c = 0;
+    const psia = this.presToPsia(valor, a);
+    switch (b) {
+      case "[psig]": {
+        c = psia - 14.7
+        return c
+      }
+      case "[psia]": {
+        c = psia;
+        return c
+      }
+      case "[barg]": {
+        c = psia / 14.50377
+        return c
+      }
+      case "[KPag]": {
+        c = psia * 6.895 - 14.7
+        return c
+      }
+      default: {
+        return c
+      }
+    }
   }
 
   presToPsia(valor: number, a: string) {
@@ -45,11 +113,11 @@ export class DimensionesService {
         return c
       }
       case "[barg]": {
-        c = valor*14.50377
+        c = valor * 14.50377
         return c
       }
       case "[KPag]": {
-        c = valor/6.895 +14.7
+        c = valor / 6.895 + 14.7
         return c
       }
       default: {
@@ -58,23 +126,49 @@ export class DimensionesService {
     }
   }
 
-  transformarDensidad(valor:number, a: string, b: string): number {
+  transformarDensidad(valor: number, a: string, b: string): number {
+    let c = 0;
+
+    return valor
+  }
+  densToGCm3(valor:number, a:string){
+    let c = 0
+    switch (a) {
+      case "[lbmol/pie3]":{
+
+      }
+      case "[lbm/pie3]":{
+
+      }
+      case "[lbm/pulg3]":{
+
+      }
+      case "[Kg/m3]":{
+
+      }
+      case "[g/cm3]":{
+
+      }
+      case "[°API]":{
+
+      }
+
+    }
+  }
+
+  transformarVolumen(valor: number, a: string, b: string): number {
     return valor
   }
 
-  transformarVolumen(valor:number, a: string, b: string): number {
+  transformarEntalpia(valor: number, a: string, b: string): number {
     return valor
   }
 
-  transformarEntalpia(valor:number, a: string, b: string): number {
+  transformarEntropia(valor: number, a: string, b: string): number {
     return valor
   }
 
-  transformarEntropia(valor:number, a: string, b: string): number {
-    return valor
-  }
-
-  transformarFlujo(valor:number, a: string, b: string): number {
+  transformarFlujo(valor: number, a: string, b: string): number {
     return valor
   }
 
