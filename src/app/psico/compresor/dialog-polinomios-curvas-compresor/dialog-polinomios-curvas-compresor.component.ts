a1d3d2ef9f8daa2eb60588ee07394f00f0d7eb1a
predycsoft <predycsoft@gmail.com>
2021-10-25 13:15:32 -0500

    zcflvdflvk

--- src/app/psico/compresor/dialog-polinomios-curvas-compresor/dialog-polinomios-curvas-compresor.component.ts
+++ src/app/psico/compresor/dialog-polinomios-curvas-compresor/dialog-polinomios-curvas-compresor.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import { DataServiceService } from 'src/app/services/data-service.service';
 import { DialogService } from 'src/app/services/dialog.service';
 
 @Component({
@@ -14,10 +15,13 @@ export class DialogPolinomiosCurvasCompresorComponent implements OnInit {
   grado: number;
   unidadFlujo: string = 'Q/N';
 
-  constructor(public dialogService: DialogService, private ngxCsvParser: NgxCsvParser) { }
+  constructor(public dialogService: DialogService, private ngxCsvParser: NgxCsvParser, private data: DataServiceService) { }
 
   csvRecords: any[] = [];
-  header = true; 
+  dataSetCP: any[] = [];
+  dataSetEfi: any[] = []
+  header = true;
+  file;
 
   ngOnInit(): void {
   }
@@ -33,7 +37,6 @@ export class DialogPolinomiosCurvasCompresorComponent implements OnInit {
         if(result instanceof NgxCSVParserError) {
         } else {
           this.csvRecords = result;
-          this.csvRecords = this.csvRecords.filter(x => x["Q/N"] != "" )
           this.armarData();
         }
       }, (error: NgxCSVParserError) => {
@@ -42,7 +45,30 @@ export class DialogPolinomiosCurvasCompresorComponent implements OnInit {
   }
 
   armarData(){
-    console.log(this.csvRecords)
+    this.dataSetCP = []
+    this.dataSetEfi = []
+    for (let index = 0; index < this.csvRecords.length; index++) {
+      const cpData = {
+        x: this.csvRecords[index]["x1"],
+        y: this.csvRecords[index]["CP"]
+      }
+      const efiData = {
+        x: this.csvRecords[index]["x2"],
+        y: this.csvRecords[index]["EFI"]
+      }
+      this.dataSetCP.push(cpData)
+      this.dataSetEfi.push(efiData)
+    }
+    this.dataSetCP = this.dataSetCP.filter(x => x.x != "")
+    this.dataSetEfi = this.dataSetEfi.filter(x => x.x != "")
+  }
+
+  @ViewChild('fileImportInput', {static: false}) myInputVariable: ElementRef;
+  reset() {
+    this.myInputVariable.nativeElement.value = '';
+    this.csvRecords = []
+    this.dataSetEfi = []
+    this.dataSetCP = []
   }
 
   eliminarCurva() {
@@ -52,4 +78,43 @@ export class DialogPolinomiosCurvasCompresorComponent implements OnInit {
     });
   }
 
+  descargarCSV(){
+    this.csvRecords = []
+    let len = 0
+    const cpLen = this.dataSetCP.length
+    const efiLen = this.dataSetEfi.length
+    if (this.dataSetCP.length > this.dataSetEfi.length){
+      len = this.dataSetCP.length
+    } else {
+      len = this.dataSetEfi.length
+    }
+    for (let index = 0; index < len; index++) {
+      let x1 = ""
+      let CP = ""
+      let x2 = ""
+      let EFI = ""
+      if  (index < cpLen) {
+        x1 = this.dataSetCP[index]["x"]
+        CP = this.dataSetCP[index]["y"]
+      } else {
+        x1 = ""
+        CP = ""
+      }
+      if  (index < efiLen) {
+        x2 = this.dataSetEfi[index]["x"]
+        EFI = this.dataSetEfi[index]["y"]
+      } else {
+        x2 = ""
+        EFI = ""
+      }
+      const obj = {
+        x1: x1,
+        CP: CP,
+        x2: x2,
+        EFI: EFI
+      }
+      this.csvRecords.push(obj)
+    }
+    this.data.downloadFile(this.csvRecords,"data-curvas",['x1','CP', 'x2', 'EFI'])
+  }
 }
