e47308c55eba47926d48be793f9d029841c057c5
predycsoft <predycsoft@gmail.com>
2021-11-03 18:41:41 -0600

    asdf

--- src/app/psico/inputs/cromatografia/cromatografia.component.ts
+++ src/app/psico/inputs/cromatografia/cromatografia.component.ts
@@ -1,32 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { DataServiceService } from 'src/app/services/data-service.service';
 import { DialogLibreriaCromatografiasComponent } from '../dialog-libreria-cromatografias/dialog-libreria-cromatografias.component';
-
-interface componente {
-  nombre: string,
-  formula: string,
-  pesoMolecular: number,
-  fraccionMolar: number;
-}
-
-class cromatografia {
-  metano: number = 0;
-  etano: number = 0;
-  propano: number = 0;
-  iButano: number = 0;
-  nButano: number = 0;
-  iPentano: number = 0;
-  nPentano: number = 0;
-  hexano: number = 0;
-  heptano: number = 0;
-  octano: number = 0;
-  nonano: number = 0;
-  decano: number = 0;
-  nitrogeno: number = 0;
-  dioxCarbono: number = 0;
-  sulfHidrogeno: number = 0;
-  aire: number = 0;
-}
 
 class propiedadesCromatografia{
     // Propiedades calculadas del gas
@@ -53,54 +28,36 @@ class propiedadesCromatografia{
 export class CromatografiaComponent implements OnInit {
 
   nombreCromatografia: string = ''
-  cr: cromatografia = new cromatografia();
+  cromatografiaOriginal = new cromatografia
+  cromatografiaNormalizada = new cromatografia
   propidadesCromatografia = new propiedadesCromatografia();
   fraccMolarTotal: number = 0;
 
-  cromatografia: componente[] = [
-    { nombre: 'Metano', formula: 'C1', pesoMolecular: 16.042, fraccionMolar: this.cr.metano},
-    { nombre: 'Etano', formula: 'C1', pesoMolecular: 30.07, fraccionMolar: this.cr.etano},
-    { nombre: 'Propano', formula: 'C1', pesoMolecular: 44.097, fraccionMolar: this.cr.propano},
-    { nombre: 'I-butano', formula: 'C1', pesoMolecular: 58.124, fraccionMolar: this.cr.iButano},
-    { nombre: 'N-butano', formula: 'C1', pesoMolecular: 58.128, fraccionMolar: this.cr.nButano},
-    { nombre: 'I-pentano', formula: 'C1', pesoMolecular: 72.151, fraccionMolar: this.cr.iPentano},
-    { nombre: 'N-pentano', formula: 'C1', pesoMolecular: 72.151, fraccionMolar: this.cr.nPentano},
-    { nombre: 'Hexano', formula: 'C1', pesoMolecular: 86.178, fraccionMolar: this.cr.hexano},
-    { nombre: 'Heptano', formula: 'C1', pesoMolecular: 100.205, fraccionMolar: this.cr.heptano},
-    { nombre: 'Octano', formula: 'C1', pesoMolecular: 114.232, fraccionMolar: this.cr.octano},
-    { nombre: 'Nonano', formula: 'C1', pesoMolecular: 128.259, fraccionMolar: this.cr.nonano},
-    { nombre: 'Decano', formula: 'C1', pesoMolecular: 142.259, fraccionMolar: this.cr.decano},
-    { nombre: 'Nitrogeno', formula: 'C1', pesoMolecular: 28.013, fraccionMolar: this.cr.nitrogeno},
-    { nombre: 'DioxCarbono', formula: 'C1', pesoMolecular: 44.13, fraccionMolar: this.cr.dioxCarbono},
-    { nombre: 'SulfHidrogeno', formula: 'C1', pesoMolecular: 34.076, fraccionMolar: this.cr.sulfHidrogeno},
-    { nombre: 'Aire', formula: 'C1', pesoMolecular: 28.966, fraccionMolar: this.cr.aire}
-  ]
-
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, public data: DataServiceService) { }
 
   ngOnInit(): void {
 
   }
 
-  normalizar() {
-    this.fraccMolarTotal = 0
-    let val = JSON.parse(JSON.stringify(this.cr))
-    Object.keys(val).forEach(key => {
-      this.fraccMolarTotal = this.fraccMolarTotal + +val[key]
-    })
-    Object.keys(val).forEach(key => {
-      val[key] = val[key]/this.fraccMolarTotal
-    })
-    this.cr= val;
-  }
+  // normalizar() {
+  //   this.fraccMolarTotal = 0
+  //   let val = JSON.parse(JSON.stringify(this.cr))
+  //   Object.keys(val).forEach(key => {
+  //     this.fraccMolarTotal = this.fraccMolarTotal + +val[key]
+  //   })
+  //   Object.keys(val).forEach(key => {
+  //     val[key] = val[key]/this.fraccMolarTotal
+  //   })
+  //   this.cr= val;
+  // }
 
-  returnZero() {
-    return 0
-  }
+  // returnZero() {
+  //   return 0
+  // }
 
-  customTrackBy(index: number, obj: any): any {
-    return index;
-  }
+  // customTrackBy(index: number, obj: any): any {
+  //   return index;
+  // }
 
   openLibreriaCromatografias(){
     const dialogRef = this.dialog.open(DialogLibreriaCromatografiasComponent, {
