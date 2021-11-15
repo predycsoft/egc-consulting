bcf6363e159b0f607a4ae034246b3ce8b298aeee
predycsoft <predycsoft@gmail.com>
2021-11-14 21:00:02 -0600

    sdafgdfg

--- src/app/psico/psico-routing.module.ts
+++ src/app/psico/psico-routing.module.ts
@@ -8,7 +8,6 @@ import { ListaProyectosComponent } from './proyectos/lista-proyectos/lista-proye
 import { PsicoDashboardComponent } from './psico-dashboard/psico-dashboard.component';
 import { ListaTrenesComponent } from '../psico/proyectos/lista-trenes/lista-trenes.component';
 import { SimulacionCampoInputComponent } from './simulacion-campo-input/simulacion-campo-input.component';
-import { SimulacionCampoComponent } from './compresor/simulacion-campo/simulacion-campo.component';
 import { ReporteSumarioSeccionComponent } from './reportes/reporte-sumario-seccion/reporte-sumario-seccion.component';
 import { ReporteSumarioInputsComponent } from './reportes/reporte-sumario-inputs/reporte-sumario-inputs.component';
 import { ReportePuntoComponent } from './reportes/reporte-punto/reporte-punto.component';
@@ -19,6 +18,8 @@ import { ListaStComponent } from './lista-st/lista-st.component';
 import { SimulacionTeoricaComponent } from './simulacion-teorica/simulacion-teorica.component';
 import { ConfigProyectoComponent } from './config-proyecto/config-proyecto.component';
 import { ConfiguracionCompresorComponent } from './compresor/configuracion-compresor/configuracion-compresor.component';
+import { SimulacionCampoListaComponent } from './compresor/simulacion-campo-lista/simulacion-campo-lista.component';
+import { SimulacionCampoDashboardComponent } from './compresor/simulacion-campo-dashboard/simulacion-campo-dashboard.component';
 
 
 const routes: Routes = [
@@ -31,14 +32,27 @@ const routes: Routes = [
     {path: '', component:ListaTrenesComponent},
     {path: 'config-proyecto', component:ConfigProyectoComponent},
     {path: 'tren/:trenTag', component: ConfiguracionTrenComponent},
-    {path: 'tren/:trenTag/lista-pruebas-eficiencia', component:ListaPeComponent},
-    {path: 'tren/:trenTag/prueba-eficiencia/:idPrueba', component:DialogSimCampoComponent},
-    {path: 'tren/:trenTag/lista-simulaciones-teoricas', component:ListaStComponent},
-    {path: 'tren/:trenTag/simulacion-teorica/:idPrueba', component:SimulacionTeoricaComponent},
+
+
     {path: 'simulacion-input/:trenTag/:equipoTag', component: SimulacionCampoInputComponent},
     {path: 'tren/:trenTag/conf/:equipoTag', component: ConfiguracionCompresorComponent},
     // {path: 'tren/:trenTag/sim-teorica', component: SimulacionTeoricaComponent},
-    {path: 'tren/:trenTag/sim-campo', component: SimulacionCampoComponent},
+
+
+    // Sim campo
+    {path: 'tren/:trenTag/sim-campo-lista', component: SimulacionCampoListaComponent},
+    {path: 'tren/:trenTag/sim-campo-dashboard', component: SimulacionCampoDashboardComponent},
+    // Sim teorica
+    {path: 'tren/:trenTag/lista-simulaciones-teoricas', component:ListaStComponent},
+    {path: 'tren/:trenTag/simulacion-teorica/:idPrueba', component:SimulacionTeoricaComponent},
+    // Prueba efic
+    {path: 'tren/:trenTag/lista-pruebas-eficiencia', component:ListaPeComponent},
+    {path: 'tren/:trenTag/prueba-eficiencia/:idPrueba', component:DialogSimCampoComponent},
+
+
+
+
+
     {path: 'tren/:trenTag/reporte-sumario-seccion', component: ReporteSumarioSeccionComponent},
     {path: 'tren/:trenTag/reporte-sumario-inputs', component: ReporteSumarioInputsComponent},
     {path: 'tren/:trenTag/reporte-punto', component: ReportePuntoComponent},
